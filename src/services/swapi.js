58977a48586dc4f341a0d9dc975623a83ebcaf92
Roma Makarov <50619943+r0mmm4k@users.noreply.github.com>
2020-04-15 12:19:08 +0300

    fix
    fix url api

--- src/services/swapi.js
+++ src/services/swapi.js
@@ -22,7 +22,7 @@ export default class SwapiService {
 	};
 
 	getPerson = async (id) => {
-		const person = await this.getResource(`/people/${id}`);
+		const person = await this.getResource(`/people/${id}/`);
 		return this._transformPerson(person);
 	};
 
@@ -34,7 +34,7 @@ export default class SwapiService {
 	};
 
 	getPlanet = async (id) => {
-		const planet = await this.getResource(`/planets/${id}`);
+		const planet = await this.getResource(`/planets/${id}/`);
 		return this._transformPlanet(planet);
 	};
 
@@ -46,7 +46,7 @@ export default class SwapiService {
 	};
 
 	getStarship = async (id) => {
-		const starship = await this.getResource(`/starships/${id}`);
+		const starship = await this.getResource(`/starships/${id}/`);
 		return this._transformStarship(starship);
 	};
 
